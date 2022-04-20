1ae0e98fc8bca3406c0fda091e39055b30457091
On2021 <dhkdrnfl4578@knou.ac.kr>
2022-04-21 04:40:12 +0900

    2022-04-21 04:39 성은작업
    준님 즉시결제 에러 해결(깃헙보고 합치기)
    => 근데 장바구니결제 != 즉시결제 (?)

---
+++ src/main/java/student/controller/STUBuyDirectController.java
@@ -0,0 +1,49 @@
+package student.controller;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import admin.model.CoBean;
+import student.model.StuCartBean;
+import student.model.StuCartDao;
+
+@Controller
+public class STUBuyDirectController {
+
+	private final String command = "buydirect.stu";
+	private String gotoPage = "redirect:/payment.stu";
+	
+	@Autowired
+	private StuCartDao scdao;
+	
+	@RequestMapping(command)
+	public String doAction(@RequestParam(value="conum", required=false ) int conum,
+							HttpSession session) {
+		
+		session.removeAttribute("cartArr");
+		
+		ArrayList<StuCartBean> cartArr = new ArrayList<StuCartBean>();
+		
+		CoBean cobean = scdao.getCourseByConum(conum);
+		
+		StuCartBean cartbean = new StuCartBean();
+		cartbean.setConame(cobean.getConame());
+		cartbean.setConum(cobean.getConum());
+		cartbean.setCoimage(cobean.getCoimage());
+		cartbean.setCoprice(cobean.getCoprice());
+		cartbean.setCosubject(cobean.getCosubject());
+		cartbean.setCoteacher(cobean.getCoteacher());
+		
+		cartArr.add(cartbean);
+		
+		session.setAttribute("cartArr", cartArr);
+		
+		return gotoPage;
+	}
+}
